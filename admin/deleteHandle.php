58a6d43cd693054fc393c3c760cd809a81375bfa
Shiron <vannguyen11a21@gmail.com>
2025-09-24 00:13:12 +0700

    Handled delete successfully

---
+++ admin/deleteHandle.php
@@ -0,0 +1,18 @@
+<?php 
+include_once('../connect.php');
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+if($id > 0){
+    $stmt = mysqli_prepare($connect,"DELETE FROM product_name WHERE id = ?");
+    mysqli_stmt_bind_param($stmt,'i',$id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    // Chuyển hướng về lại danh sách với thông báo
+    echo "<script>alert('Delete sucessfilly'); window.location.href='readHandle.php';</script>";
+    exit;
+} else {
+    echo "<script>alert('ID is unable'); window.location.href='readHandle.php';</script>";
+    exit;
+}
+?>
--- admin/readHandle.php
+++ admin/readHandle.php
@@ -1,13 +1,30 @@
-<?php 
+<?php
 include_once('../connect.php');
+
+// Xử lý thêm sản phẩm
+if (isset($_POST['add'])) {
+    $nameProduct = isset($_POST['nameProduct']) ? trim($_POST['nameProduct']) : '';
+    if ($nameProduct !== '') {
+        $statement = mysqli_prepare($connect, "INSERT INTO product_name (nameProduct) VALUES (?)");
+        if ($statement) {
+            mysqli_stmt_bind_param($statement, "s", $nameProduct);
+            mysqli_stmt_execute($statement);
+            mysqli_stmt_close($statement);
+        }
+        // Reload lại trang để cập nhật bảng
+        header("Location: readHandle.php");
+        exit();
+    } else {
+        echo "<script>alert('Product name cannot be empty');</script>";
+    }
+}
 
 // Lấy dữ liệu từ CSDL
 $sql = "SELECT * FROM product_name ORDER BY id ASC";
 $result = mysqli_query($connect,$sql);
 if(!$result){
-    die("Error: Could not able to execute $sql ." / mysqli_error($connect));
+    die("Error: Could not able to execute $sql ." . mysqli_error($connect));
 }
-
 ?>
 
 
@@ -34,8 +51,11 @@ if(!$result){
                 <td><?php echo $row['id']; ?></td>
                 <td><label for=""> <?php echo htmlspecialchars($row['nameProduct']); ?></label></td>
 
-                <td><button type="submit" onclick="return confirm('Are you to delete?'); ">Delete</button>
-                    <button type="submit">Update</button>
+                <td>
+                    <div class="btn-group">
+                        <a href="./deleteHandle.php?id=<?php echo $row['id']; ?>" class="btn btn-de" onclick="return comfirm('Are you to delete?')">Delete</a>
+                        <a href="./updateHandle.php?id=<?php echo $row['id']; ?>" class="btn btn-up">Update</a>
+                    </div>    
                 </td>
             </tr>
 
